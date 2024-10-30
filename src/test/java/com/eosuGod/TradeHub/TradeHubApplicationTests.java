7627b485845f3349c5e643268b175c4962f8971b
leeflection <leeflection@gmail.com>
2024-10-30 21:35:10 +0900

    init

---
+++ src/test/java/com/eosuGod/TradeHub/TradeHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eosuGod.TradeHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradeHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
