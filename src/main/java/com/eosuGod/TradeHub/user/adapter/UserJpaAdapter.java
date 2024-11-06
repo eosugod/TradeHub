6c229fc622827dcef6c653a36dc5ecabf814b492
leeflection <leeflection@gmail.com>
2024-11-06 21:30:55 +0900

    feat: InfraSturucture 구현
    port & adapter 패턴을 활용하여 jpa 어댑터 구현
    
    Closes #1

---
+++ src/main/java/com/eosuGod/TradeHub/user/adapter/UserJpaAdapter.java
@@ -0,0 +1,31 @@
+package com.eosugod.tradehub.user.adapter;
+
+import com.eosugod.tradehub.user.domain.UserDomain;
+import com.eosugod.tradehub.user.dto.request.RequestCreateUserDto;
+import com.eosugod.tradehub.user.mapper.UserMapper;
+import com.eosugod.tradehub.user.port.UserPort;
+import com.eosugod.tradehub.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class UserJpaAdapter implements UserPort {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDomain save(RequestCreateUserDto userDto) {
+        return UserMapper.persistenceToDomain(userRepository.save(UserMapper.dtoTopersistence(userDto)));
+    }
+
+    @Override
+    public boolean existsByAccount(String account) {
+        return userRepository.existsByAccount(account);
+    }
+
+    @Override
+    public boolean existsByNickName(String nickName) {
+        return userRepository.existsByNickName(nickName);
+    }
+
+}
