8170bfc6b534459612588932d38b24e022b38aa1
leeflection <leeflection@gmail.com>
2024-11-06 21:28:33 +0900

    feat: mapper 생성
    dto -> domain
    domain -> dto
    persistence -> domain
    domain -> persistence
    
    BREAKING CHANGE: 각종 변환 mapper 메서드 구현
    
    Closes #1

---
+++ src/main/java/com/eosuGod/TradeHub/user/mapper/UserMapper.java
@@ -0,0 +1,66 @@
+package com.eosugod.tradehub.user.mapper;
+
+import com.eosugod.tradehub.user.domain.UserDomain;
+import com.eosugod.tradehub.user.dto.request.RequestCreateUserDto;
+import com.eosugod.tradehub.user.dto.response.ResponseUserDto;
+import com.eosugod.tradehub.user.entity.Users;
+import com.eosugod.tradehub.user.vo.Address;
+import com.eosugod.tradehub.user.vo.Money;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class UserMapper {
+
+    public static Users domainToPersistence(UserDomain userDomain) {
+        return Users.builder()
+                    .name(userDomain.getName())
+                    .account(userDomain.getAccount())
+                    .nickName(userDomain.getNickName())
+                    .cash(userDomain.getCash())
+                    .address(userDomain.getAddress())
+                    .build();
+    }
+
+    public static UserDomain persistenceToDomain(Users usersEntity) {
+        return UserDomain.builder()
+                         .id(usersEntity.getId())
+                         .name(usersEntity.getName())
+                         .account(usersEntity.getAccount())
+                         .address(usersEntity.getAddress())
+                         .account(usersEntity.getAccount())
+                         .cash(usersEntity.getCash())
+                         .nickName(usersEntity.getNickName())
+                         .build();
+    }
+
+    public static UserDomain dtoToDomain(RequestCreateUserDto dto) {
+        return UserDomain.builder()
+                         .account(dto.account())
+                         .cash(new Money())
+                         .name(dto.name())
+                         .nickName(dto.nickName())
+                         .address(new Address(dto.locationCode()))
+                         .build();
+    }
+
+    public static Users dtoTopersistence(RequestCreateUserDto dto) {
+        return Users.builder()
+                    .name(dto.name())
+                    .account(dto.account())
+                    .nickName(dto.nickName())
+                    .cash(new Money())
+                    .address(new Address(dto.locationCode()))
+                    .build();
+    }
+
+    public static ResponseUserDto domainToDto(UserDomain userDomain) {
+        return ResponseUserDto.builder()
+                              .id(userDomain.getId())
+                              .name(userDomain.getName())
+                              .account(userDomain.getAccount())
+                              .nickName(userDomain.getNickName())
+                              .cash(userDomain.getCash().getValue())
+                              .address(userDomain.getAddress().getValue())
+                              .build();
+    }
+}
