28ddcaacf2b89118d475c42c224d7e6fd1b72a04
leeflection <leeflection@gmail.com>
2024-11-07 22:42:33 +0900

    feat: 유저 미 존재시 적용할 Exception 정의
    유저 미 존재시 적용할 Exception 정의
    
    Closes #4

--- src/main/java/com/eosuGod/TradeHub/util/ExceptionCode.java
+++ src/main/java/com/eosuGod/TradeHub/util/ExceptionCode.java
@@ -6,7 +6,8 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum ExceptionCode {
-    EXIST_ACCOUNT(1001, "이미 등록된 계좌입니다."), EXIST_NICKNAME(1002, "이미 등록된 닉네임입니다.");
+    EXIST_ACCOUNT(1001, "이미 등록된 계좌입니다."), EXIST_NICKNAME(1002, "이미 등록된 닉네임입니다."),
+    NOT_EXIST_USER(4001, "존재하지 않는 유저입니다.");
 
     private final int code;
     private final String message;
