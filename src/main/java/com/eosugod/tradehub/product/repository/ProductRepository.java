058268f6c37050900d669004873108fd5153569e
SuHyeonEo <tngus7520@gmail.com>
2024-11-19 13:01:05 +0900

    feat: 상품 검색 조회 레포지토리 구현
    상품 검색 조회 레포지토리 구현
    
    Closes #7
    
    [skip ci]

--- src/main/java/com/eosugod/tradehub/product/repository/ProductRepository.java
+++ src/main/java/com/eosugod/tradehub/product/repository/ProductRepository.java
@@ -1,8 +1,10 @@
 package com.eosugod.tradehub.product.repository;
 
 import com.eosugod.tradehub.product.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
-
+    Page<Product> findByTitleOrText(String titleKeyword, String textKeyword, Pageable pageable);
 }
