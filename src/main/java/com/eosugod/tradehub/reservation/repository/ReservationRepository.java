1345ee0ca8404f4a874bb31ce072ec7168b6e303
SuHyeonEo <tngus7520@gmail.com>
2024-11-21 22:03:31 +0900

    feat: 예약 조회 컨트롤러 구현
    예약 단일 조회,
    해당 상품의 예약 전체 조회 구현
    
    Closes #14

--- src/main/java/com/eosugod/tradehub/reservation/repository/ReservationRepository.java
+++ src/main/java/com/eosugod/tradehub/reservation/repository/ReservationRepository.java
@@ -1,10 +1,14 @@
 package com.eosugod.tradehub.reservation.repository;
 
 import com.eosugod.tradehub.reservation.entity.Reservation;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
     boolean existsByBuyerIdAndProductId(Long buyerId, Long productId);
     boolean existsByProductId(Long productId);
+    Page<Reservation> findAllByProductId(Long productId, Pageable pageable);
 }
