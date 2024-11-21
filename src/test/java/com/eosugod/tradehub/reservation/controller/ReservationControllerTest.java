420fca3ba1391a15231330c4a7f16d44d0486872
SuHyeonEo <tngus7520@gmail.com>
2024-11-21 22:03:53 +0900

    feat: 예약 조회 컨트롤러 테스트
    예약 단일 조회,
    해당 상품의 예약 전체 조회 구현
    
    Closes #14

--- src/test/java/com/eosugod/tradehub/reservation/controller/ReservationControllerTest.java
+++ src/test/java/com/eosugod/tradehub/reservation/controller/ReservationControllerTest.java
@@ -10,11 +10,14 @@ import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.Arrays;
 
 import static org.mockito.BDDMockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
@@ -36,10 +39,11 @@ public class ReservationControllerTest {
         // given
         Long productId = 1L;
         RequestCreateReservationDto requestDto = RequestCreateReservationDto.builder()
+                                                                            .productId(productId)
                                                                             .buyerId(2L)
                                                                             .price(BigDecimal.valueOf(10000))
                                                                             .locationCode("1234567890")
-                                                                            .confirmedAt(LocalDateTime.now())
+                                                                            .confirmedAt(LocalDateTime.now().plusHours(1))
                                                                             .build();
 
         ResponseReservationDto responseDto = ResponseReservationDto.builder()
@@ -49,14 +53,14 @@ public class ReservationControllerTest {
                                                                    .sellerId(3L)
                                                                    .price(BigDecimal.valueOf(10000))
                                                                    .locationCode("1234567890")
-                                                                   .confirmedAt(LocalDateTime.now())
+                                                                   .confirmedAt(LocalDateTime.now().plusHours(1))
                                                                    .build();
 
         Mockito.when(reservationService.createReservation(any(RequestCreateReservationDto.class)))
                .thenReturn(responseDto);
 
         // when
-        mockMvc.perform(post("/products/{productId}/reservations", productId)
+        mockMvc.perform(post("/reservations")
                        .contentType(MediaType.APPLICATION_JSON)
                        .content(objectMapper.writeValueAsString(requestDto))
                        .accept(MediaType.APPLICATION_JSON))
@@ -69,5 +73,34 @@ public class ReservationControllerTest {
 
         // then
         then(reservationService).should().createReservation(any(RequestCreateReservationDto.class));
+    }
+
+    @Test
+    @DisplayName("해당 상품 전체 예약 조회")
+    void testGetAllReservations_Success() throws Exception {
+        // given: 상품에 대한 예약 목록
+        Long productId = 1L;
+        Page<ResponseReservationDto> page = new PageImpl<>(Arrays.asList(
+                ResponseReservationDto.builder()
+                                      .id(1L)
+                                      .productId(productId)
+                                      .buyerId(2L)
+                                      .price(BigDecimal.valueOf(10000))
+                                      .build()
+        ));
+
+        // when
+        when(reservationService.getAllReservations(eq(productId), anyInt(), anyInt()))
+                .thenReturn(page);
+
+        // then
+        mockMvc.perform(get("/reservations/products/{productId}", productId)
+                       .param("page", "0")
+                       .param("size", "20"))
+               .andExpect(status().isOk())
+               .andExpect(jsonPath("$.content[0].id").value(1L))
+               .andExpect(jsonPath("$.content[0].productId").value(productId))
+               .andExpect(jsonPath("$.content[0].price").value(10000))
+               .andReturn();
     }
 }
